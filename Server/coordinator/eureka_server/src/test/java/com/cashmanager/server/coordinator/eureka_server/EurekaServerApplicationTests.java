567d4ffbf57232579708ca594cfbf56781e15699
MeloLawson <melodie.lawson@epitech.eu>
2023-12-04 11:57:36 +0100

    feat:create api gateway
    api gateway creation with eureka server to manage the microservices
    
    linked #35

---
+++ Server/coordinator/eureka_server/src/test/java/com/cashmanager/server/coordinator/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashmanager.server.coordinator.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
