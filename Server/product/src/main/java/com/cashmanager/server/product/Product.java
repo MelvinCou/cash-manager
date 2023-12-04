7ca25e23fe5580745bd5686a72cc4f3a8823786e
MansourWolou <woloumansour@gmail.com>
2023-12-04 15:33:07 +0100

    feature(Server): Delete usless repertory
    Intellig create an out repertory with contain the wall project
    
    Linked #39

--- Server/product/src/main/java/com/cashmanager/server/product/Product.java
+++ Server/product/src/main/java/com/cashmanager/server/product/Product.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 @ToString
 @RequiredArgsConstructor
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "products")
--- out/production/cash-manager/terminal/lib/features/product/domain/repository/product.dart
+++
@@ -1,7 +0,0 @@
-import 'package:terminal/common/data_state.dart';
-import 'package:terminal/features/product/domain/entity/order.dart';
-
-abstract class ProductRepository {
-  Future<DataState> getProduct();
-  Future<DataState> makeOrder(Order order);
-}
