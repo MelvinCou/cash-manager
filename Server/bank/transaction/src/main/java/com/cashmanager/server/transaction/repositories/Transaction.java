87a15c2a4d28bf130e3dcb39fa8f32de783a68e6
MelvinCou <melvin.courjaud@epitech.eu>
2023-12-05 10:04:35 +0100

    feat: transaction module

---
+++ Server/bank/transaction/src/main/java/com/cashmanager/server/transaction/repositories/Transaction.java
@@ -0,0 +1,38 @@
+package com.cashmanager.server.transaction.repositories;
+
+import com.cashmanager.server.account.repositories.PaymentMethod;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Entity(name = "transactions")
+public class Transaction {
+    @Id
+    private UUID id;
+
+    @ToString.Exclude
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "payment_method_id", nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated
+    @Column(name = "status", nullable = false)
+    private EnumTransactionStatus status;
+
+    @Column(name = "amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "receiver", nullable = false)
+    private String receiver;
+}
