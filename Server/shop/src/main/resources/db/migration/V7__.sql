7fdd0e36fc208c4b6d93135957a26538cbd42f4e
MansourWolou <woloumansour@gmail.com>
2023-12-05 10:38:49 +0100

    feature(Server): clear migration
    Only one migration file is required
    
    Linked #39

--- Server/shop/src/main/resources/db/migration/V7__.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE ordered_orders
-    ADD product_id UUID;
-
-ALTER TABLE ordered_orders
-    ALTER COLUMN product_id SET NOT NULL;
-
-ALTER TABLE ordered_orders
-    ADD CONSTRAINT FK_ORDERED_ORDERS_ON_PRODUCT FOREIGN KEY (product_id) REFERENCES products (product_id);
