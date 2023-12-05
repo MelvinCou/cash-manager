5c89639c71e8271b29dafb916beec125de0e4fa2
MansourWolou <woloumansour@gmail.com>
2023-12-05 09:39:29 +0100

    feature(Server): Added methods for objects comparaison
    .idea is already ignored and we don't have to ignore .jpb folder
    Added methods for object comparaison
    
    Linked #39

--- Server/shop/src/main/java/com/cashmanager/server/shop/Ordered_orders.java
+++ Server/shop/src/main/java/com/cashmanager/server/shop/Ordered_orders.java
@@ -2,7 +2,9 @@ package com.cashmanager.server.shop;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.util.Objects;
 import java.util.UUID;
 @Getter
 @Setter
@@ -25,4 +27,19 @@ public class Ordered_orders {
     @JoinColumn(name = "order_id", nullable=false)
     private Order orders;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Ordered_orders that = (Ordered_orders) o;
+        return getOrdered_orders_id() != null && Objects.equals(getOrdered_orders_id(), that.getOrdered_orders_id());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
