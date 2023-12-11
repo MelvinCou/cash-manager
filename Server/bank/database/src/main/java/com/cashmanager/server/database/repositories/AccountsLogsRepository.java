c6fbef16787e105409332f362edf106dad05fe52
MelvinCou <melvin.courjaud@epitech.eu>
2023-12-11 10:38:21 +0100

    feature(bank): Add missing repository

--- Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/AccountsLogsRepository.java
+++ Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/AccountsLogsRepository.java
@@ -1,9 +1,12 @@
 package com.cashmanager.server.database.repositories;
 
 import com.cashmanager.server.database.entities.AccountsLogs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AccountsLogsRepository extends JpaRepository<AccountsLogs, UUID> {
+    Logger logger = LoggerFactory.getLogger(AccountsLogsRepository.class);
 }
