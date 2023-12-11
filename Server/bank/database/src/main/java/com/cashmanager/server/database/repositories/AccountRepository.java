67bc437decc5311428eaad7393e8feefca250b84
MelvinCou <melvin.courjaud@epitech.eu>
2023-12-11 16:30:04 +0100

    feature(bank): Add helpers in repositories

--- Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/AccountRepository.java
+++ Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/AccountRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+    // get all accounts from a user
+    Iterable<Account> findAllByUser_Id(UUID id);
 }
