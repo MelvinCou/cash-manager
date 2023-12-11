96ee6bd3572a24da0cdd66dcbe3abd4798d4fc2d
MansourWolou <woloumansour@gmail.com>
2023-12-11 03:17:51 +0100

    Revert "experiment(terminal): Qr code scanning plugin"
    This reverts commit e4be06491494d0281bfa62c440c244a374cb706b.

--- terminal/lib/features/transaction/domain/usecase/get_check_data.dart
+++ terminal/lib/features/transaction/domain/usecase/get_check_data.dart
@@ -1,53 +1,12 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:terminal/common/usecase.dart';
 import 'package:terminal/features/transaction/domain/entity/check.dart';
-import 'dart:convert';
 
-class GetCheckData implements Equatable, UseCase<Check, BarcodeCapture> {
-  late final String? qrCodeRawValue;
+class GetCheckData implements Equatable, UseCase<Check, void> {
   @override
-  Future<Check> call({BarcodeCapture? params}) {
-    late Check checkObject;
-    try {
-      detect(params!);
-      //! don't know if json will be well decode to check object
-      checkObject = json.decode(qrCodeRawValue!);
-    } catch (e) {
-      debugPrint("______________________________________________");
-      debugPrint(e as String?);
-    }
-
-    return Future(() => checkObject);
-  }
-
-  void detect(BarcodeCapture capture) {
-    // fit: BoxFit.contain,
-    final List<Barcode> barcodes = capture.barcodes;
-    for (final barcode in barcodes) {
-      /// check barcodes is not empty. IF it is no qr code detected : do nothing , user can scan a qr code anytime he want
-      /// check barcode.rawValue: if empty qr code data type is bad -> returen message telling user to search for good qr code
-      /// check format = BarcodeFormat.qrCode
-      ///
-      // String checkData = "{'AccountNumber': '578985', 'Amount': 25}";
-      if (barcode.format == BarcodeFormat.qrCode) {
-        if (barcode.rawValue is String) {
-          qrCodeRawValue = barcode.rawValue;
-          debugPrint(
-              'Barcode found! format:  ${barcode.format} ; rawValue: $qrCodeRawValue');
-        } else {
-          debugPrint("qr code raw value is not a string");
-        }
-      } else {
-        // bad qr code format
-        debugPrint('bad qr code format');
-      }
-    }
-  }
-
-  Check parseFromJsonToCheck(Map<String, dynamic> json) {
-    return Check(amount: json["amount"], checkNumber: json["checkNumber"]);
+  Future<Check> call({void params}) {
+    // TODO: implement call
+    throw UnimplementedError();
   }
 
   @override
