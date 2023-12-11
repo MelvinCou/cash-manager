87049a439abc56cceca88147d141ea90bda3a317
MelvinCou <melvin.courjaud@epitech.eu>
2023-12-11 16:30:04 +0100

    feature(bank): Add helpers in repositories

--- Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/AccountsLogsRepository.java
+++ Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/AccountsLogsRepository.java
@@ -1,12 +1,11 @@
 package com.cashmanager.server.database.repositories;
 
 import com.cashmanager.server.database.entities.AccountsLogs;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AccountsLogsRepository extends JpaRepository<AccountsLogs, UUID> {
-    Logger logger = LoggerFactory.getLogger(AccountsLogsRepository.class);
+    // get all logs from an account
+    Iterable<AccountsLogs> findAllByAccount_Id(UUID id);
 }
--- Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/TransactionLogRepository.java
+++ Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/TransactionLogRepository.java
@@ -1,9 +1,16 @@
 package com.cashmanager.server.database.repositories;
 
 import com.cashmanager.server.database.entities.TransactionLog;
+import com.cashmanager.server.database.enums.LogSeverity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface TransactionLogRepository extends JpaRepository<TransactionLog, UUID> {
+    // find by severity
+    Iterable<TransactionLog> findAllBySeverity(LogSeverity severity);
+
+    // get all logs from transaction
+    Iterable<TransactionLog> findAllByTransaction_Id(UUID id);
+
 }
