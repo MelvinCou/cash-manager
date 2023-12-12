20d0bc9c443c96c8a02b4687a3250b205b274e00
MeloLawson <melodie.lawson@epitech.eu>
2023-12-12 15:36:47 +0100

    feature:list of products
    draft
    
    linked #16

--- terminal/lib/main.dart
+++ terminal/lib/main.dart
@@ -9,17 +9,18 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Shop',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My Shop'),
     );
   }
 }
