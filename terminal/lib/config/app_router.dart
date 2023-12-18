de3386ab727fd34de9ef53daa512d14ab5f0b4da
MeloLawson <melodie.lawson@epitech.eu>
2023-12-18 10:02:24 +0100

    feat:view products list
    add the layout of home page and products list page
    
    linked #16

--- terminal/lib/config/app_router.dart
+++ terminal/lib/config/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:terminal/features/home/home_page.dart';
-import 'package:terminal/features/product/presentation/pages/list_of_products/products_view.dart';
+import 'package:terminal/features/product/app/pages/list_of_products/products_view.dart';
+
 import '../features/layout/layout_view.dart';
 
 part 'app_router.gr.dart';
