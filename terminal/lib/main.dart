a24f1f667dccbe68b5022bc3d6031fa51cd73da6
MansourWolou <woloumansour@gmail.com>
2023-12-18 16:46:57 +0100

    feat(terminal): check feature done
    THe ui for scanning the Qr code is done too.
    Does not handle interaction between payement method choice and validation page
    
    Linked #42

--- terminal/lib/main.dart
+++ terminal/lib/main.dart
@@ -1,12 +1,6 @@
-// ignore_for_file: avoid_print
-
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:terminal/features/transaction/domain/entity/check.dart';
-import 'package:terminal/features/transaction/domain/usecase/get_check_data.dart';
+import 'package:terminal/features/transaction/presentation/check_scan.dart';
 import 'package:terminal/injection_container.dart';
-import 'package:flutter_zxing/flutter_zxing.dart';
 
 void main() async {
   await initializeDependencies();
@@ -41,14 +35,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ReaderWidget(
-        onScan: (result) async {
-          //* valild json : {"checkNumer":468516,"amount":85}
-          GetCheckData getCheckData = GetCheckData();
-          print(getCheckData(params: result));
-        },
-      ),
-    );
+    return const Scaffold(body: CheckScan());
   }
 }
