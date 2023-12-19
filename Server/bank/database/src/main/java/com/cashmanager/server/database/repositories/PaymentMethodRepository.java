332e09d6ddb7852ac8bfc3ad5b91b5c8d066982d
Paul <irvoaspaul@outlook.fr>
2023-12-19 10:55:39 +0100

    feat(repository): add function to get PaymentMethod

--- Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/PaymentMethodRepository.java
+++ Server/bank/database/src/main/java/com/cashmanager/server/database/repositories/PaymentMethodRepository.java
@@ -3,7 +3,9 @@ package com.cashmanager.server.database.repositories;
 import com.cashmanager.server.database.entities.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, UUID> {
+    Optional<PaymentMethod> findByCreditCardNumberAndCvc(String creditCardNumber, String cvc);
 }
