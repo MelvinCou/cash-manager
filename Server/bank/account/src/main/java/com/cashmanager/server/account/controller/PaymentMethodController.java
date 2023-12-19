6471ef95b350b083bf3a8aae2475adf6b8b45864
Paul <irvoaspaul@outlook.fr>
2023-12-19 15:42:58 +0100

    feat(transaction): add logic on transaction step

--- Server/bank/account/src/main/java/com/cashmanager/server/account/controller/PaymentMethodController.java
+++ Server/bank/account/src/main/java/com/cashmanager/server/account/controller/PaymentMethodController.java
@@ -1,23 +1,22 @@
 package com.cashmanager.server.account.controller;
 
+import com.cashmanager.server.account.verification.CheckVerification;
+import com.cashmanager.server.account.verification.CreditCardVerification;
 import com.cashmanager.server.common.dto.PaymentMethodDto;
-import com.cashmanager.server.database.entities.PaymentMethod;
-import com.cashmanager.server.database.enums.AccountState;
-import com.cashmanager.server.database.repositories.PaymentMethodRepository;
+import com.cashmanager.server.database.entity.PaymentMethod;
+import com.cashmanager.server.database.repository.PaymentMethodRepository;
 import org.springframework.stereotype.Controller;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Optional;
 
 /**
- * Controller in charge of managing payment method
+ * Controller in charge of managing and checking payment method
  */
 @Controller
 public class PaymentMethodController {
 
     private PaymentMethodRepository paymentMethodRepository;
+    private CreditCardVerification creditCardVerification;
 
     /**
      * Used to get and validate if PaymentMethod is viable
@@ -26,32 +25,23 @@ public class PaymentMethodController {
      * @return PaymentMethod object
      */
     public Optional<PaymentMethod> checkPaymentMethodViability(PaymentMethodDto paymentMethodDto) {
-        Optional<PaymentMethod> paymentMethod = paymentMethodRepository.findByCreditCardNumberAndCvc(paymentMethodDto.getCreditCardNumber(), paymentMethodDto.getCvc());
 
-        if (paymentMethod.isPresent()) {
-            if (paymentMethod.get().getAccount().getState().equals(AccountState.ACTIVE)) {
+        Optional<PaymentMethod> paymentMethod = Optional.empty();
+        if (CreditCardVerification.isACreditCard(paymentMethodDto)) {
 
-                ZonedDateTime currentUtcDateTime = ZonedDateTime.now(ZoneId.of("UTC"));
-
-                LocalDateTime validityDate = paymentMethod.get().getValidityDate();
-                ZonedDateTime validityDateUtc = validityDate.atZone(ZoneId.of("UTC"));
-
-                if (validityDateUtc.isBefore(currentUtcDateTime)) { // Everything is ok
-                    return paymentMethod;
-                } else if (validityDateUtc.isAfter(currentUtcDateTime)) { // case of validityDate is expired
-                    // TODO -> Create log, case of validityDate is expired
-                    return Optional.empty();
-                } else { // case of validityDate is equal to currentDate
-                    // TODO -> Create log, case of validityDate is equal to currentDate
-                    return Optional.empty();
-                }
-            } else { // case of Account is INACTIVE
-                // TODO -> Create log, case of Account is INACTIVE
-                return Optional.empty();
+            paymentMethod = paymentMethodRepository.findByCreditCardNumberAndCvc(paymentMethodDto.getCreditCardNumber(), paymentMethodDto.getCvc());
+            if (paymentMethod.isEmpty()) {
+                // TODO -> Create log, case of paymentMethod credit card isn't found
             }
-        } else { // case of PaymentMethod isn't found
-            // TODO -> Create log, case of PaymentMethod isn't found
-            return Optional.empty();
+        } else if (CheckVerification.isACheck(paymentMethodDto)) {
+
+            paymentMethod = paymentMethodRepository.findByCheckNumber(paymentMethodDto.getCheckNumber());
+            if (paymentMethod.isEmpty()) {
+                // TODO -> Create log, case of paymentMethod check isn't found
+            }
+        } else {
+            // TODO -> Create log, case of PaymentMethod isn't check or card
         }
+        return paymentMethod;
     }
 }
