62a6004e8a549bd3e8f7402506a8b774b95da6ff
MeloLawson <melodie.lawson@epitech.eu>
2023-12-19 13:00:40 +0100

    feature: add bloc products
    implementation of the bloc architecture for the state management of the product view
    
    linked #16

---
+++ terminal/lib/features/product/domain/usecase/get_all_products.dart
@@ -0,0 +1,27 @@
+import 'package:terminal/injection_container.dart';
+
+import '../../../../common/data_state.dart';
+import '../../../../common/usecase.dart';
+import '../entity/product.dart';
+import '../repository/product.dart';
+
+class GetAllProducts implements UseCase<List<Product>, void> {
+  final ProductRepository productRepository = getIt();
+
+  GetAllProducts();
+  @override
+  Future<List<Product>> call({void params}) async {
+    DataState result = await productRepository.getAllProducts();
+    try {
+      if (result is DataSuccess) {
+        //return product list
+        return result.data;
+      } else {
+        throw Exception("Didn't get products ; ${result.errorMessage}");
+      }
+    } catch (err) {
+      // return empty array
+      return [];
+    }
+  }
+}
