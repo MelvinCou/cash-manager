3ca2ca3fdadd8b3db91dbd08264eb2abdca59c14
MeloLawson <melodie.lawson@epitech.eu>
2023-12-19 15:48:36 +0100

    fix : ci failed
    corrections in the test files and apis.dart
    
    linked #16

--- terminal/lib/common/apis.dart
+++ terminal/lib/common/apis.dart
@@ -1,5 +1,5 @@
 class Apis {
   static const _baseUrl = 'http://10.0.2.2:8080/api';//TODO à changer
-  static const fetchedProducts = _baseUrl + '/products';//TODO à changer
+  static const fetchedProducts = '$_baseUrl/products';//TODO à changer
 
 }
