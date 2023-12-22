f92eef114a6dd362256bd9334116c951673f9f23
Paul <irvoaspaul@outlook.fr>
2023-12-22 10:18:29 +0100

    fix(repository): add constraint to remove error

--- Server/bank/account/src/main/java/com/cashmanager/server/account/AccountApplication.java
+++ Server/bank/account/src/main/java/com/cashmanager/server/account/AccountApplication.java
@@ -2,8 +2,10 @@ package com.cashmanager.server.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.cashmanager.server")
 public class AccountApplication {
 
 	public static void main(String[] args) {
