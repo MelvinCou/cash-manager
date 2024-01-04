335c9976a87991391add83492d42c4e09308e1b5
Paul <irvoaspaul@outlook.fr>
2024-01-04 12:17:36 +0100

    fix(transaction): change call to mapper and add functions

--- Server/bank/transaction/src/main/java/com/cashmanager/server/transaction/controller/TransactionController.java
+++ Server/bank/transaction/src/main/java/com/cashmanager/server/transaction/controller/TransactionController.java
@@ -6,12 +6,15 @@ import com.cashmanager.server.common.dto.PaymentMethodDto;
 import com.cashmanager.server.common.dto.TransactionDto;
 import com.cashmanager.server.common.enumeration.TransactionStatus;
 import com.cashmanager.server.database.entity.PaymentMethod;
+import com.cashmanager.server.database.entity.Transaction;
 import com.cashmanager.server.database.entity.TransactionLog;
 import com.cashmanager.server.database.enumeration.LogSeverity;
+import com.cashmanager.server.database.mapper.PaymentMethodMapper;
 import com.cashmanager.server.database.mapper.TransactionMapper;
+import com.cashmanager.server.database.repository.AccountRepository;
 import com.cashmanager.server.database.repository.TransactionLogRepository;
+import com.cashmanager.server.database.repository.TransactionRepository;
 import com.cashmanager.server.transaction.model.TransactionHolder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +29,10 @@ import java.util.Optional;
 public class TransactionController {
 
     private TransactionLogRepository transactionLogRepository;
+    private TransactionRepository transactionRepository;
+    private AccountRepository accountRepository;
     private PaymentMethodService paymentMethodService;
     private AccountService accountService;
-    private TransactionMapper transactionMapper;
 
     /**
      * Starting point from mediator for payment method and transaction steps
@@ -42,16 +46,53 @@ public class TransactionController {
 
         Optional<PaymentMethod> paymentMethod = paymentMethodService.checkPaymentMethodViability(paymentMethodDto, transactionDto);
         if (paymentMethod.isPresent()) {
-            if (accountService.checkAccountValidity(paymentMethod.get().getAccount(), transactionDto)) {
-                transactionLogRepository.save(new TransactionLog(null, transactionMapper.transactionDtoToTransaction(transactionDto), LocalDateTime.now(), LogSeverity.INFO, "payment method and account validation ok"));
+            if (accountService.checkAccountValidity(accountRepository.findById(paymentMethod.get().getAccount().getId()), transactionDto)) {
                 transactionDto.setTransactionStatus(TransactionStatus.PAYMENT_ACCEPTED);
+
+                saveTransaction(paymentMethodDto, transactionDto, transactionHolder);
+                saveTransactionLog(transactionDto, "payment method and account validation ok");
             } else {
-                transactionLogRepository.save(new TransactionLog(null, transactionMapper.transactionDtoToTransaction(transactionDto), LocalDateTime.now(), LogSeverity.ERROR, "account validation nok"));
                 transactionDto.setTransactionStatus(TransactionStatus.CANCELED);
+
+                saveTransaction(paymentMethodDto, transactionDto, transactionHolder);
+                saveTransactionLog(transactionDto, "account validation nok");
             }
         } else {
-            transactionLogRepository.save(new TransactionLog(null, transactionMapper.transactionDtoToTransaction(transactionDto), LocalDateTime.now(), LogSeverity.ERROR, "payment method nok"));
             transactionDto.setTransactionStatus(TransactionStatus.CANCELED);
+
+            saveTransaction(paymentMethodDto, transactionDto, transactionHolder);
+            saveTransactionLog(transactionDto, "payment method nok");
         }
+    }
+
+    /**
+     * Private method used to save the current transaction
+     * @param paymentMethodDto  -
+     * @param transactionDto    -
+     * @param transactionHolder - Object containing payment method and amount of transaction
+     */
+    private void saveTransaction(PaymentMethodDto paymentMethodDto, TransactionDto transactionDto, TransactionHolder transactionHolder) {
+
+        transactionRepository.save(new Transaction(
+                null,
+                PaymentMethodMapper.INSTANCE.paymentMethodDtoToPaymentMethod(paymentMethodDto),
+                transactionDto.getTransactionStatus(),
+                transactionHolder.getAmount(),
+                LocalDateTime.now(),
+                ""));
+    }
+
+    /**
+     * Private method used to save the transaction log
+     * @param transactionDto    -
+     * @param message           - String indicate the reason of log creation
+     */
+    private void saveTransactionLog(TransactionDto transactionDto, String message) {
+
+        transactionLogRepository.save(new TransactionLog(
+                null,
+                TransactionMapper.INSTANCE.transactionDtoToTransaction(transactionDto),
+                LocalDateTime.now(),
+                LogSeverity.INFO, "payment method and account validation ok"));
     }
 }
