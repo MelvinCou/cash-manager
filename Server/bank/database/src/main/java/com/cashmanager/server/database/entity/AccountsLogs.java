29c26fdfc2709f4693159882f5baaeca454a52b3
Paul <irvoaspaul@outlook.fr>
2024-01-04 14:23:11 +0100

    test(accountlog): verify if account log can be build with maven into pull request

--- Server/bank/database/src/main/java/com/cashmanager/server/database/entity/AccountsLogs.java
+++ Server/bank/database/src/main/java/com/cashmanager/server/database/entity/AccountsLogs.java
@@ -1,9 +1,7 @@
 package com.cashmanager.server.database.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -11,6 +9,8 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Entity(name = "accounts_logs")
 public class AccountsLogs {
     @Id
@@ -27,4 +27,49 @@ public class AccountsLogs {
 
     @Column(name = "date", nullable = false)
     private LocalDateTime date;
+
+    /**
+     * Constructor used to create error log
+     * @param account of user
+     * @param message error
+     * @return AccountsLogs object
+     */
+    public static AccountsLogs error(Account account, String message) {
+        return new AccountsLogs(
+                null,
+                account,
+                message,
+                LocalDateTime.now()
+        );
+    }
+
+    /**
+     * Constructor used to create warn log
+     * @param account of user
+     * @param message warn
+     * @return AccountsLogs object
+     */
+    public static AccountsLogs warn(Account account, String message) {
+        return new AccountsLogs(
+                null,
+                account,
+                message,
+                LocalDateTime.now()
+        );
+    }
+
+    /**
+     * Constructor used to create info log
+     * @param account of user
+     * @param message info
+     * @return AccountsLogs object
+     */
+    public static AccountsLogs info(Account account, String message) {
+        return new AccountsLogs(
+                null,
+                account,
+                message,
+                LocalDateTime.now()
+        );
+    }
 }
