c9231879df831cdc9ef8435f19f9c17e86b57972
Paul <irvoaspaul@outlook.fr>
2024-01-07 20:25:59 +0100

    fix(application): add component-scan

--- Server/bank/account/src/main/java/com/cashmanager/server/account/AccountApplication.java
+++ Server/bank/account/src/main/java/com/cashmanager/server/account/AccountApplication.java
@@ -2,10 +2,12 @@ package com.cashmanager.server.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.cashmanager.server")
+@ComponentScan({"com.cashmanager.server.account.service", "com.cashmanager.server.database.repository"})
 public class AccountApplication {
 
 	public static void main(String[] args) {
--- Server/bank/transaction/src/main/java/com/cashmanager/server/transaction/TransactionApplication.java
+++ Server/bank/transaction/src/main/java/com/cashmanager/server/transaction/TransactionApplication.java
@@ -2,9 +2,16 @@ package com.cashmanager.server.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
-		"com.cashmanager.server.database.repository"})
+		"com.cashmanager.server.database.repository",
+		"com.cashmanager.server.transaction"})
+@ComponentScan("com.cashmanager.server.account.service")
+@EnableJpaRepositories(basePackages = "com.cashmanager.server")
+@EntityScan("com.cashmanager.server")
 public class TransactionApplication {
 
 	public static void main(String[] args) {
